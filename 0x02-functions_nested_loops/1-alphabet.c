2aa638614a1c5951ae5a4dc2a5f9f46a6ad8c078
mimz <njokimimz@gmail.com>
2023-09-13 16:27:10 -0700

    LOWERCASE

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print  alphabet. 
+ * All in lower case
+ */
+void print_alphabet(void);
+{
+	char lowercase:
+		for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+			_putchar(lowercase);
+
+	_putchar('\n');
+}
