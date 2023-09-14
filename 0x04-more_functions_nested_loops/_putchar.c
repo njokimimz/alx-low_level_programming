486188dd75615514ed9775b6d852047a2dd77231
mimz <njokimimz@gmail.com>
2023-09-14 05:30:45 -0700

    supper

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _putchar - writes the char c to stdout
+ *
+ *
+ * Return: On success 1.
+ * on error -1 is returned, and error rectified
+ */
+int_putchar(char c);
+{
+	return (write(1, &c, 1))
+}
+
